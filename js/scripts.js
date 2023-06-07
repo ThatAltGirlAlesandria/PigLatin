742a6b367ea6f29eec276a73aae60904cd589986
ThatAltGirlAlesandria <134242341+ThatAltGirlAlesandria@users.noreply.github.com>
2023-06-07 13:58:50 -0700

    making a list of function to run for tests and to build for UI after.

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//Business Logic:
+
+1. array to check for vowels
+2. array to check for constonants
+3. array to check for qu at the beginning of a word only
+4. array that will move words around add way or ay to the end of words.
+5. function that takes the form and pushes it through to the UI after running through the arrays
+
+//User Interface: 
+function for form submission 
+window.onload for the "final product"
